96294e0ace93d5bce6de28c3a390e795d0b9bad9
sylvia villegas <sylvia@svillegas.com>
2023-07-22 21:58:15 -0400

    install and configure astro-sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sanity from "astro-sanity";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: "iugkqal6",
+      dataset: "production",
+      apiVersion: "2023-02-08",
+      useCdn: false,
+    }),
+  ],
+});
