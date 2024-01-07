df392217247c8a9b243bc574dfab8a25e53fca2c
sylvia villegas <sylvia@svillegas.com>
2024-01-07 07:37:19 -0500

    move all date formatting to utils, replace with <Time>

--- src/utils/time.js
+++ src/utils/time.js
@@ -8,6 +8,24 @@ export const formatDate = (date, formatName) => {
         timeZone: "America/Toronto",
       }).format(new Date(date));
 
+    // e.g. Jun 6, 2023
+    case "MMMDYYYY":
+      return new Intl.DateTimeFormat("en-CA", {
+        dateStyle: "medium",
+        timeZone: "America/Toronto",
+      }).format(new Date(date));
+
+    // e.g. 1/8/2024, 10:13 a.m.
+    case "qqq":
+      return new Intl.DateTimeFormat("en-CA", {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        timeZone: "America/Toronto",
+      }).format(new Date(date));
+
     default:
       return date;
   }
